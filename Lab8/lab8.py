dd4512112193b19dee7b0d45527995f28784a519
nathanbuyr <G00421082@atu.ie>
2024-11-18 16:10:57 +0000

    6) Subplotted both images

--- Lab8/lab8.py
+++ Lab8/lab8.py
@@ -3,17 +3,30 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+fig = plt.figure(figsize=(10, 7))
+
+rows = 2
+columns = 1
+
 # Load the input image
 image = cv2.imread('ATU.jpg')
-cv2.imshow('Original', image)
-cv2.waitKey(0)
-
 # Use the cvtColor() function to grayscale the image
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('Grayscale', gray_image)
-cv2.waitKey(0)  
+fig.add_subplot(rows, columns, 1) 
+  
+# showing image 
+plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB)) 
+plt.axis('off') 
+plt.title("First") 
 
-# Window shown waits for any key pressing event
-cv2.destroyAllWindows()
+# Adds a subplot at the 2nd position 
+fig.add_subplot(rows, columns, 2) 
+  
+# showing image 
+plt.imshow(cv2.cvtColor(gray_image,cv2.COLOR_BGR2RGB)) 
+plt.axis('off') 
+plt.title("Second")
+
+plt.show()
 
