c00b073c1e13cc445b1786c4db5b5e63c5652bd1
nathanbuyr <G00421082@atu.ie>
2024-11-18 16:44:11 +0000

    10-11)Sobel x,y and added together are imlemented

--- Lab8/lab8.py
+++ Lab8/lab8.py
@@ -3,11 +3,6 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-fig = plt.figure(figsize=(10, 7))
-
-rows = 2
-columns = 2
-
 # Load the input image
 image = cv2.imread('ATU.jpg')
 # Use the cvtColor() function to grayscale the image
@@ -16,35 +11,30 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurredimage =  cv2.GaussianBlur(gray_image,(3, 3),0)
 # 13x13 blur
 moreblurredimage = cv2.GaussianBlur(gray_image,(13, 13),0)
+# Sobel images
+sobelHorizontal = cv2.Sobel(blurredimage,cv2.CV_64F,1,0,ksize=5) # x dir
+sobelVertical = cv2.Sobel(blurredimage,cv2.CV_64F,0,1,ksize=5) # y dir
+sobel_added = sobelVertical + sobelVertical
 
-fig.add_subplot(rows, columns, 1) 
-  
-# showing image 
-plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB)) 
-plt.axis('off') 
-plt.title("Original") 
+plt.subplot(3, 3,1),plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
 
-# Adds a subplot at the 2nd position 
-fig.add_subplot(rows, columns, 2) 
-  
-# showing image 
-plt.imshow(cv2.cvtColor(gray_image,cv2.COLOR_BGR2RGB)) 
-plt.axis('off') 
-plt.title("Grayscale")
+plt.subplot(3, 3,2),plt.imshow(gray_image, cmap = 'gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 
-fig.add_subplot(rows, columns, 3) 
-  
-# showing image 
-plt.imshow(cv2.cvtColor(blurredimage,cv2.COLOR_BGR2RGB)) 
-plt.axis('off') 
-plt.title("3x3 Blur")
+plt.subplot(3, 3,3),plt.imshow(blurredimage, cmap = 'gray')
+plt.title('3x3 Blur'), plt.xticks([]), plt.yticks([])
 
-fig.add_subplot(rows, columns, 4) 
-  
-# showing image 
-plt.imshow(cv2.cvtColor(moreblurredimage,cv2.COLOR_BGR2RGB)) 
-plt.axis('off') 
-plt.title("13x13 Blur")
+plt.subplot(3, 3,4),plt.imshow(sobelHorizontal, cmap = 'gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(3, 3,5),plt.imshow(sobelVertical, cmap = 'gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(3, 3,6),plt.imshow(sobel_added, cmap = 'gray')
+plt.title('13x13 Blur'), plt.xticks([]), plt.yticks([])
+
 
 plt.show()
+
 
