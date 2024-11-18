67e85ba4b04616a74bdd5056f5326fe83b1e6d14
nathanbuyr <G00421082@atu.ie>
2024-11-18 16:46:50 +0000

    12)Canny implemented

--- Lab8/lab8.py
+++ Lab8/lab8.py
@@ -14,7 +14,10 @@ moreblurredimage = cv2.GaussianBlur(gray_image,(13, 13),0)
 # Sobel images
 sobelHorizontal = cv2.Sobel(blurredimage,cv2.CV_64F,1,0,ksize=5) # x dir
 sobelVertical = cv2.Sobel(blurredimage,cv2.CV_64F,0,1,ksize=5) # y dir
-sobel_added = sobelVertical + sobelVertical
+sobel_added = sobelHorizontal + sobelVertical
+# Canny images
+canny = cv2.Canny(image,100,200)
+
 
 plt.subplot(3, 3,1),plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
@@ -22,17 +25,17 @@ plt.title('Original'), plt.xticks([]), plt.yticks([])
 plt.subplot(3, 3,2),plt.imshow(gray_image, cmap = 'gray')
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(3, 3,3),plt.imshow(blurredimage, cmap = 'gray')
-plt.title('3x3 Blur'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(3, 3,4),plt.imshow(sobelHorizontal, cmap = 'gray')
+plt.subplot(3, 3,3),plt.imshow(sobelHorizontal, cmap = 'gray')
 plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(3, 3,5),plt.imshow(sobelVertical, cmap = 'gray')
+plt.subplot(3, 3,4),plt.imshow(sobelVertical, cmap = 'gray')
 plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(3, 3,6),plt.imshow(sobel_added, cmap = 'gray')
-plt.title('13x13 Blur'), plt.xticks([]), plt.yticks([])
+plt.subplot(3, 3,5),plt.imshow(sobel_added, cmap = 'gray')
+plt.title('Sobel added'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(3, 3,6),plt.imshow(canny, cmap = 'gray')
+plt.title('Canny'), plt.xticks([]), plt.yticks([])
 
 
 plt.show()
