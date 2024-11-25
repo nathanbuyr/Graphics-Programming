f17f9bdeccaebeaecff8e64e171077df06079d50
nathanbuyr <G00421082@atu.ie>
2024-11-25 16:15:17 +0000

    9-12) Shi tomasi algorithim displayed

--- Lab9/lab9.py
+++ Lab9/lab9.py
@@ -15,6 +15,17 @@ imgHarris = copy.deepcopy(image)
 # Harris corner detection
 dst = cv2.cornerHarris(gray_image, 2, 3, 0.04)
 
+# Shi Tomasi algorithim
+corners = cv2.goodFeaturesToTrack(gray_image,100,0.01,10)
+
+#Deep copying for Shi Tomasi
+imgShiTomasi = copy.deepcopy(image)
+
+# GFTT corner loop
+for i in corners:
+    x,y = i.ravel()
+    cv2.circle(imgShiTomasi,(x,y),3,(0, 255, 0),-1)
+
 #Plotting detected Harris corners
 threshold = 0.05; #number between 0 and 1
 for i in range(len(dst)):
@@ -22,13 +33,14 @@ for i in range(len(dst)):
         if dst[i][j] > (threshold*dst.max()):
             cv2.circle(imgHarris,(j,i),3,(123, 55, 100),-1)
 
-plt.subplot(2, 1,1),plt.imshow(gray_image, cmap = 'gray')
+plt.subplot(1, 3,1),plt.imshow(gray_image, cmap = 'gray')
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(2, 1,2),plt.imshow((imgHarris), cmap = 'gray')
+plt.subplot(1, 3,2),plt.imshow((imgHarris), cmap = 'gray')
 plt.title('Harris corner detection'), plt.xticks([]), plt.yticks([])
 
-
+plt.subplot(1, 3,3),plt.imshow((imgShiTomasi), cmap = 'gray')
+plt.title('GFTT / Shi Tomasi corner detection'), plt.xticks([]), plt.yticks([])
 
 plt.show()
 
